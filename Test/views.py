60fddbba8bcefebaebc2b0450096c4bff1d8b265
Zobject <18920354189@163.com>
2017-05-27 11:44:55 +0800

    Summer

--- Test/views.py
+++ Test/views.py
@@ -32,7 +32,7 @@ def insert(request):
     Brief =request.GET['brief']
     Audio = request.GET['Audio']
     Suitable =request.GET['Suitable']
-    if(collection.find({"Musicurl":Musicurl}).count()>0):
+    if(collection.find({"Name":Name}).count()>0):
         return  render(request,'success.html')
     doc={'Name':Name,'Musicurl':Musicurl,'Author':Author,'Press':Press,'Column':Column,'Recommended':Recommended,'Probation':Probation,'Cover':Cover,'Brief':Brief,'Audio':Audio,'Suitable':Suitable,'Upload':datetime.datetime.now(),'Degree':0,'Free':0}
     print doc
@@ -42,12 +42,35 @@ def insert(request):
 
 @csrf_exempt
 def booklist(request):
-    if request.method=="GET":
-        date=list(collection.find())
+    if request.method=="POST":
+        date=[]
+        content=JSONParser().parse(request)
+        page=content.get('page')
+        target=content.get('target')
+        sum=content.get('sum')
+       # page=request.GET['page']
+        booksum = collection.find().count()
+        if int(target)==1:
+            #sum=collection.find().count()
+            if int(page)==1:
+                sumindatabase = collection.find().count()
+                date.append({"sum": sumindatabase})
+            if  booksum < 5  :
+                 date = list(collection.find())
+            else:
+                if booksum-5*(int(page))<=0:
+                    date = list(collection.find().limit((booksum-5*(int(page)))%5))
+                else:
+                    date=list(collection.find().skip(booksum-5*(int(page))).limit(5))
+        if int(target)==0:
+            if int(sum) < booksum:
+                date=list(collection.find().skip(sum-1))
+                date.append({"sum": booksum})
         for i in  date:
             i['Upload'] = str(i.get('Upload'))
-    c=list(reversed(date))
-    print str(c)
+
+        c=list(reversed(date))
+        print str(c)
     return HttpResponse(json.dumps(c,default=json_util.default),status=200,content_type='application/json')
 
 
