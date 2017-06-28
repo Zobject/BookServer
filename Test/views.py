41cca1009a7df0949543f6e51643a87e3e593c0c
Zobject <18920354189@163.com>
2017-06-28 11:48:57 +0800

    Summary

--- Test/views.py
+++ Test/views.py
@@ -295,11 +295,11 @@ def listenlist(request):
                  date = list(collection.find())
             else:
                 #判断如果是最后一页根据具体剩余数目进行返回
-                if int(page)==int(sum)/5+1:
-                    if int(sum)>(int(page)-1)*5:
-                        date = list(collection.find().skip().limit((int(booksum)-5*(int(page)))%5))
+                if int(page)==booksum/5+1:
+                    if int(booksum) > (int(page) - 1) * 5:
+                        date = list(collection.find().limit((int(booksum) - 5 * (int(page))) % 5))
                     else:
-                        date=[]
+                        date = []
                 #如果请求的页数大于 数据库页数返回空
                 elif int(page)>booksum/5+1:
                     date=[]
